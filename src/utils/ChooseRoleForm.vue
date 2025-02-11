56c2a1b2d82a526bc0a7008770a5d737310a3750
William <Williamalim410@gmail.com>
2025-02-11 21:46:31 +0700

    Fix tampilan chooseroleform.vue

--- src/utils/ChooseRoleForm.vue
+++ src/utils/ChooseRoleForm.vue
@@ -1,58 +1,110 @@
 <template>
-  <v-container class="custom-container d-flex flex-column align-center py-10">
-    <v-img src="@/assets/images/logo-kolab-aja.png" height="40" class="my-8" />
-    <v-col>
-      <h1 class="text-h3 font-weight-bold">Pilih Peranmu!</h1>
-      <p class="desc text-h5">Masuk sebagai?</p>
-    </v-col>
+  <!-- Jika device berukuran kecil (mobile) -->
+  <template v-if="smAndDown">
+    <div class="mobile-view">
+      <v-container class="custom-container d-flex flex-column align-center py-10">
+        <v-img src="@/assets/images/logo-kolab-aja.png" height="40" class="my-8" />
+        <v-col>
+          <h1 class="text-h3 font-weight-bold">Pilih Peranmu!</h1>
+          <p class="desc text-h5">Masuk sebagai?</p>
+        </v-col>
 
-    <v-row class="gap-4">
-      <v-col v-for="item in roleList" :key="item.roleId" cols="12" md="4">
-        <v-card
-          :class="{ 'active-card': selectedRoleId === item.roleId }"
-          class="pa-5 border role-card"
-          variant="flat"
-          @click="changeRole(item.roleId)"
-        >
-          <v-card-title class="d-flex justify-left pb-6">
-            <v-icon
-              :icon="item.icon"
-              size="x-large"
-              :color="selectedRoleId === item.roleId ? 'white' : ''"
-            ></v-icon>
-          </v-card-title>
-          <v-card-title
-            :class="{ 'text-white': selectedRoleId === item.roleId }"
-            class="text-h5"
-            >{{ item.name }}</v-card-title
-          >
-          <v-card-text
-            :class="selectedRoleId === item.roleId ? 'role-desc-active' : 'role-desc'"
-          >
-            {{ item.description }}
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
+        <v-row class="gap-4">
+          <v-col v-for="item in roleList" :key="item.roleId" cols="12" md="4">
+            <v-card :class="{ 'active-card': selectedRoleId === item.roleId }" class="pa-5 border role-card"
+              variant="flat" @click="changeRole(item.roleId)">
+              <v-card-title class="d-flex justify-left pb-6">
+                <v-icon :icon="item.icon" size="x-large"
+                  :color="selectedRoleId === item.roleId ? 'white' : ''"></v-icon>
+              </v-card-title>
+              <v-card-title :class="{ 'text-white': selectedRoleId === item.roleId }" class="text-h5">
+                {{ item.name }}
+              </v-card-title>
+              <v-card-text :class="selectedRoleId === item.roleId ? 'role-desc-active' : 'role-desc'">
+                {{ item.description }}
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
 
-    <!-- Action Button and Sign In Link -->
-    <v-col class="action-section mt-8 text-center">
-      <v-btn class="btn-role text-h6 text-capitalize mb-4" @click="handleNext">
-        <p class="text-white m-0">Masuk Sebagai {{ selectedRoleName }}</p>
-        <v-icon class="ml-1" color="white" icon="mdi-arrow-right" />
-      </v-btn>
-      <p>
-        Sudah punya akun?
-        <a href="/Login" class="sign-up-link font-weight-bold" style="color: #8a76d4"
-          >Sign In</a
-        >
-      </p>
-    </v-col>
-  </v-container>
+        <!-- Tombol Aksi dan Link Sign In -->
+        <v-col class="action-section mt-8 text-center">
+          <v-btn class="btn-role text-h6 text-capitalize mb-4" @click="handleNext">
+            <p class="text-white m-0">Masuk Sebagai {{ selectedRoleName }}</p>
+            <v-icon class="ml-1" color="white" icon="mdi-arrow-right" />
+          </v-btn>
+          <p>
+            Sudah punya akun?
+            <a href="/Login" class="sign-up-link font-weight-bold" style="color: #8a76d4">
+              Sign In
+            </a>
+          </p>
+        </v-col>
+      </v-container>
+    </div>
+  </template>
+
+  <!-- Jika device berukuran desktop -->
+  <template v-else>
+    <div class="desktop-view">
+      <v-container class="mt-auto mb-auto custom-container d-flex flex-column ga-10">
+        <v-img src="@/assets/images/logo-kolab-aja.png" position="left" height="40"
+          class="mt-n16 mb-16 ml-md-n10 ml-sm-0" />
+        <v-col>
+          <h1 class="text-h3 font-weight-bold">Pilih Peranmu!</h1>
+          <p class="desc text-h5">Masuk sebagai?</p>
+        </v-col>
+
+        <v-row>
+          <v-col v-for="item in roleList" :key="item.roleId">
+            <!-- Jika role belum dipilih -->
+            <v-card v-if="selectedRoleId != item.roleId" variant="flat" class="pa-5 border role-card fixed-width h-100"
+              @click="changeRole(item.roleId)">
+              <v-card-title class="d-flex justify-left pb-12">
+                <v-icon :icon="item.icon" size="x-large"></v-icon>
+              </v-card-title>
+              <v-card-title class="text-h5">{{ item.name }}</v-card-title>
+              <v-card-text class="role-desc">
+                {{ item.description }}
+              </v-card-text>
+            </v-card>
+            <!-- Jika role terpilih -->
+            <v-card v-if="selectedRoleId == item.roleId" variant="flat"
+              class="pa-5 border role-card fixed-width h-100 active-card" @click="changeRole(item.roleId)">
+              <v-card-title class="d-flex justify-left pb-12">
+                <v-icon :icon="item.icon" size="x-large" color="white"></v-icon>
+              </v-card-title>
+              <v-card-title class="text-h5 text-white">{{ item.name }}</v-card-title>
+              <v-card-text class="role-desc-active">
+                {{ item.description }}
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+
+        <!-- Tombol Aksi dan Link Sign In -->
+        <v-col class="action-section">
+          <v-btn class="btn-role text-h6 text-capitalize mb-3" @click="handleNext">
+            <p class="text-white">Masuk Sebagai {{ selectedRoleName }}</p>
+            <v-icon class="ml-1" color="white" icon="mdi-arrow-right" />
+          </v-btn>
+          <p class="ml-5">
+            Sudah punya akun?
+            <a href="/Login" class="sign-up-link font-weight-bold" style="color: #8a76d4">
+              Sign In
+            </a>
+          </p>
+        </v-col>
+      </v-container>
+    </div>
+  </template>
 </template>
 
 <script setup>
 import { ref, computed } from "vue";
+import { useDisplay } from "vuetify";
+
+const { smAndDown } = useDisplay();
 
 const roleList = [
   {
@@ -93,40 +145,38 @@ const handleNext = () => {
 </script>
 
 <style scoped>
-* {
+/* ==================== */
+/* Styling untuk Mobile */
+/* ==================== */
+.mobile-view * {
   font-family: "Outfit", sans-serif;
 }
 
-.custom-container {
+.mobile-view .custom-container {
   max-width: 1200px;
   padding: 2rem;
 }
 
-.border {
-  border: 0.2rem solid rgba(123, 97, 220, 0.25);
-  border-radius: 8px;
-}
-
-.desc {
+.mobile-view .desc {
   font-size: 1.2rem;
   color: rgba(34, 25, 67, 0.6);
 }
 
-.role-desc {
+.mobile-view .role-desc {
   font-size: 1rem;
   color: rgba(34, 25, 67, 0.7);
 }
 
-.role-desc-active {
+.mobile-view .role-desc-active {
   color: #dee4f0;
 }
 
-.active-card {
+.mobile-view .active-card {
   background-color: #221943;
   color: #fff;
 }
 
-.btn-role {
+.mobile-view .btn-role {
   min-height: 3rem;
   padding: 0.5rem 2rem;
   border-radius: 50px;
@@ -137,26 +187,88 @@ const handleNext = () => {
   justify-content: center;
 }
 
-.text-white {
+.mobile-view .text-white {
   color: #ffffff !important;
 }
 
-.m-0 {
+.mobile-view .m-0 {
   margin: 0;
 }
 
-h1,
-p,
-.v-icon {
+.mobile-view h1,
+.mobile-view p,
+.mobile-view .v-icon {
   color: #221943;
 }
 
-.v-card-title {
+.mobile-view .v-card-title {
   font-weight: 600;
 }
 
 @media (max-width: 600px) {
-  .text-center {
+  .mobile-view .text-center {
+    text-align: center;
+  }
+}
+
+/* ====================== */
+/* Styling untuk Desktop  */
+/* ====================== */
+.desktop-view * {
+  font-family: "Outfit", sans-serif;
+}
+
+.desktop-view .desc {
+  font-size: 1vw;
+  line-height: 1.5;
+  color: rgba(34, 25, 67, 0.5);
+}
+
+.desktop-view .role-desc {
+  font-size: 14px;
+  color: rgba(34, 25, 67, 0.5) !important;
+}
+
+.desktop-view .role-desc-active {
+  color: rgba(222, 228, 240, 1) !important;
+}
+
+.desktop-view .active-card {
+  background: rgba(34, 25, 67, 1);
+}
+
+.desktop-view .btn-role {
+  height: 4vw !important;
+  color: #fff;
+  text-transform: capitalize;
+  padding: 0.7vw 2vw;
+  border-radius: 500vw;
+  background: #46377d;
+}
+
+.desktop-view h1,
+.desktop-view p,
+.desktop-view .v-icon {
+  color: #221943;
+}
+
+.desktop-view .v-card-title {
+  font-weight: 600;
+}
+
+.desktop-view .v-card-text {
+  font-size: 1vw;
+  color: rgba(34, 25, 67, 0.2);
+}
+
+@media (min-width: 1920px) {
+  .desktop-view .custom-container {
+    max-width: 1200px;
+  }
+}
+
+@media (max-width: 600px) {
+  .desktop-view .text-center.text-sm-left {
     text-align: center;
   }
 }
